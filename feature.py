dbc7e0a512436e6d68336b55f6fbd443b4798e75
alia-asl <aliabdollahiasl@gmail.com>
2024-05-08 14:07:16 +0330

    raw data added

--- feature.py
+++ feature.py
@@ -3,26 +3,25 @@ import pickle
 import os
 import numpy as np
 import pandas as pd
+from typing import Union
 
-def extract_features(verbose:int=0, log_file=True, window_size=1):
+def extract_features(verbose:int=0, log_file=True, window_size=1, use_raw=False):
     # Creating dataframe of all of them
     # `window` | `mean` | `std` | `max` | `min` | `label`
     LABELS = {1: 'baseline', 2: 'stress', 3: 'amusement', 4: 'meditation'}
     freqs = {'chest': {sensor: 700 for sensor in {'ACC', 'ECG', 'EMG', 'EDA', 'Temp', 'Resp'}},
             'wrist': {'ACC': 32, 'BVP': 64, 'EDA': 4, 'TEMP': 4}}
+    MIN_FREQ = 4
+    
 
-    window_size = window_size # in second
-    def infer_label(labels:np.ndarray):
+    def infer_label(labels:np.ndarray) -> int:
         counts = defaultdict(lambda: 0)
         for item in labels:
             counts[item] += 1
-        max_key, max_value = 0, 0
-        for item in counts:
-            if counts[item] > max_value:
-                max_key, max_value = item, counts[item]
+        max_key = max(counts, key=lambda x: counts[x])
         return max_key
 
-    if log_file:log:str = ""
+    log:str = ""
     for subject in range(2, 18):
         if subject == 12:
             continue
@@ -30,37 +29,54 @@ def extract_features(verbose:int=0, log_file=True, window_size=1):
             data = pickle.load(pklfile, encoding='bytes')
         if verbose:print(f"Data of S{subject} loaded!")
         if log_file:log += f"Data of S{subject} loaded!\n"
-        os.makedirs(f"WESAD/S{subject}/data", exist_ok=True)
+        if use_raw:
+            os.makedirs(f"WESAD/S{subject}/raw_data", exist_ok=True)
+        else:    
+            os.makedirs(f"WESAD/S{subject}/manual_data", exist_ok=True)
 
         for bdevice in data[b'signal'].keys():
             for bsensor in data[b'signal'][bdevice]:
                 device = bdevice.decode()
                 sensor = bsensor.decode()
-                df_dict = {'window': [], 'mean': [], 'std': [], 'min': [], 'max': [], 'label': []}
+                if use_raw:
+                    df_dict:dict[str, list[float]] = {f"{i}": [] for i in range(MIN_FREQ)}
+                    df_dict['window'] = []
+                    df_dict['label'] = []
+                else:
+                    df_dict:dict[str, list[float]] = {'window': [], 'mean': [], 'std': [], 'min': [], 'max': [], 'label': []}
                 freq = freqs[device][sensor] * window_size
                 raw_data:np.ndarray = data[b'signal'][bdevice][bsensor]
                 raw_data = raw_data.reshape((len(raw_data)//freq, freq, -1))
                 i = 0
                 for window in raw_data:
                     # window is a np.ndarray of the desired size
-                    df_dict['window'] = i
                     seconds_elapsed = i * window_size * 700
                     i += 1
                     labels = data[b'label'][seconds_elapsed:seconds_elapsed + window_size * 700]
                     label = infer_label(labels)
                     if label not in LABELS.keys():
                         continue # it would be same for all sets
+                    df_dict['window'].append(i)
                     df_dict['label'].append(label)
-                    for feature in ['mean', 'std', 'max', 'min']:
-                        df_dict[feature].append(getattr(window, feature)(axis=0).sum())
+
+                    if use_raw:
+                        downsampled = window[::len(window) // MIN_FREQ]
+                        for second in range(MIN_FREQ):
+                            df_dict[f"{second}"].append(downsampled[second].mean())
+                    else:
+                        for feature in ['mean', 'std', 'max', 'min']:
+                            df_dict[feature].append(getattr(window, feature)(axis=0).sum())
                     if verbose > 2:
                         print(end=f'\r{device}_{sensor}-> {i}/{len(raw_data)}')
                         if log_file:
                             log += f'\r{device}_{sensor}-> {i}/{len(raw_data)}\n'
-
+                
                 df = pd.DataFrame(df_dict)
                 df.set_index('window')
-                df.to_csv(f'WESAD/S{subject}/data/{device}_{sensor}.csv', index=False)
+                if use_raw:
+                    df.to_csv(f'WESAD/S{subject}/raw_data/{device}_{sensor}.csv', index=False) 
+                else:
+                    df.to_csv(f'WESAD/S{subject}/manual_data/{device}_{sensor}.csv', index=False)
                 if verbose > 1:print(f'{device}_{sensor}-> {df.shape}')
                 if log_file:log += f'{device}_{sensor}-> {df.shape}\n'
                 
--- training.py
+++ training.py
@@ -1,3 +1,4 @@
+import sklearn
 from sklearn.base import ClassifierMixin
 from sklearn.model_selection import ParameterGrid, train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
@@ -6,13 +7,14 @@ import numpy as np
 import os
 import random
 import time
+from typing import Callable, Union
 
 class Trainer:
     """
     A class to train models on the WESAD dataset\\
     The dataset features should be first extracted and put it in the `X`
     """
-    def __init__(self, models_params:dict[ClassifierMixin, dict[str, list]], X:pd.DataFrame, y:pd.DataFrame, split=True) -> None:
+    def __init__(self, models_params:dict[Callable, dict[str, list]], X:pd.DataFrame, y:pd.DataFrame, split:bool=True) -> None:
         """
         Parameters
         -----
@@ -24,14 +26,14 @@ class Trainer:
         `y`: Data Frame\\
         the label of each window
         """
-        self.models:list[GridSearchCV] = []
-        for model in models_params:
+        self.models = []
+        for model in models_params.keys():
             self.models.append(GridSearchCV(model(), models_params[model], scoring='f1_macro', cv=5))
         if split:
             self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, stratify=y)
         else:
-            self.X_train, self.X_test, self.y_train, self.y_test = X.copy(), None, y.copy(), None
-        self._best_estimator = None
+            self.X_train, self.X_test, self.y_train, self.y_test = X.copy(), pd.DataFrame([]), y.copy(), pd.DataFrame([])
+        self._best_estimator:sklearn.base.BaseEstimator
         self._f1_avg = 0.0
     
     def fit(self, verbose:int=0) -> None:
@@ -42,17 +44,18 @@ class Trainer:
         for model in self.models:
             if verbose:
                 print(end=f'Training {model.estimator}')
-                start_time = time.time()
+                start_time:float = time.time()
             model.fit(self.X_train, self.y_train)
             if verbose:
                 print(f"\rTraining {model.estimator} completed in {time.time() - start_time} secs")
 
             
             if model.best_score_ > self._f1_avg:
-                self._f1_avg, self._best_estimator = model.best_score_, model.best_estimator_
+                self._f1_avg:float = model.best_score_
+                self._best_estimator:sklearn.base.BaseEstimator = model.best_estimator_
         
 
-    def predict_all(self, verbose:int=0, X_test=None) -> dict[ClassifierMixin, np.array]:
+    def predict_all(self, verbose:int=0, X_test=None) -> dict[ClassifierMixin, np.ndarray]:
         if X_test == None:
             X_test = self.X_test
         preds = {}
@@ -76,7 +79,7 @@ class Trainer:
     
 
 class WESADTrainer:
-    def __init__(self, models_params:dict[ClassifierMixin, dict[str, list]], base_folder='WESAD/', filenames:list[str]=None) -> None:
+    def __init__(self, models_params:dict[Callable, dict[str, list]], sensor:str, base_folder:str='WESAD/', data_prefix='raw', subjects=[]) -> None:
         """
         Parameters
         ----
@@ -87,103 +90,83 @@ class WESADTrainer:
         files must be in this location: <`base_folder`>/<subject_id>/data/<filename>\\
         the default is 
         """
-        if filenames == None:
-            filenames = ['chest_ACC', 'chest_ECG', 'chest_EDA', 'chest_EMG', 'chest_Resp', 'chest_Temp', 
-                         'wrist_ACC', 'wrist_BVP', 'wrist_EDA', 'wrist_TEMP']
-        self.base_folder = base_folder
-        self.filenames = filenames
+        self.path = base_folder + "%s/" + data_prefix + "_data/" + sensor + ".csv"
         self.models_params = models_params
-        self._best_estimator = {}
-        self.X_train, self.y_train = {}, {}
+        self.subjects = subjects
         
         
     def predict_all(self, verbose=0):
         """
         Parameters:
         -----
-        `test`: the test subject
-
+        
         """
+        total_res:dict[str, Union[float, dict[str, float]]] = {i: {'precision': 0, 'recall': 0, 'f1-score': 0} for i in ['1', '2', '3', '4', 'weighted avg', 'macro avg']}
+        total_res['accuracy'] = 0.0
         # for testNo in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17]:
-        for testNo in [2, 3, 4]:
+        for testNo in self.subjects:
             test = f"S{testNo}"
             if verbose:print(f"Testing {test}")
-            self.load_data(test=test, verbose=verbose) # self.sensors_dfs set to dictionary of DFs
-
-            if verbose:
-                print({sen: self.sensors_dfs[sen].shape for sen in self.sensors_dfs.keys()})
-            self.fit(verbose=verbose) # apres, models are in self._best_estimator
-            test_sensors = {sensor: [] for sensor in self.filenames}
-            for filename in self.filenames:
-                test_sensors[filename].append(pd.read_csv(self.base_folder + test + '/data/' + filename + '.csv'))
-            test_sensors_dfs = {sensor: pd.concat(test_sensors[sensor]) for sensor in test_sensors}
-            if verbose:
-                print({sen: (test_sensors_dfs[sen].shape, test_sensors_dfs[sen].columns) for sen in test_sensors_dfs.keys()})
-            report = self.report(test_sensors_dfs)
-            self.X_test = test_sensors_dfs
+            inputs:pd.DataFrame = self.load_data(test=test, verbose=verbose) # a pd.DataFrame
+            self.fit(df=inputs, verbose=verbose) # apres, models are in self._best_estimator
+            test_df = pd.read_csv(self.path % test)
+            report = self.report(test_df)
+            self.X_test = test_df
             self.y_pred = self.prediction
 
             if verbose:print(f"Report of {test}: {report}")
+            for key1 in report.keys():
+                if isinstance(report[key1], float):
+                    total_res[key1] += report[key1]
+                else:
+                    for key2 in report[key1].keys():
+                        if key2 != 'support':
+                            total_res[key1][key2] += report[key1][key2]
+            
+        for key1 in report.keys():
+            if isinstance(report[key1], float):
+                total_res[key1] /= len(self.subjects)
+            else:
+                for key2 in report[key1].keys():
+                    if key2 != 'support':
+                        total_res[key1][key2] /= len(self.subjects)
+        return total_res
+            
+            
+
 
     def load_data(self, test, verbose:int=0):
-        sensors = {sensor: [] for sensor in self.filenames}
-        
-        for s in os.listdir(self.base_folder):
+        inputs:list[pd.DataFrame] = []
+        for s_num in self.subjects:
+            s = f"S{s_num}"
             if '.' in s and s != test: #subjects are in the folders
                 continue
-            for filename in self.filenames:
-                sensors[filename].append(pd.read_csv(self.base_folder + s + '/data/' + filename + '.csv'))
-            if verbose:print(end=f'\rSubject #{s} data loaded')
+            inputs.append(pd.read_csv(self.path % s))
         if verbose:
             print()
-        self.sensors_dfs = {sensor: pd.concat(sensors[sensor]) for sensor in sensors} # a dictionary, keys are sensors, values are the data for all the subjects
-        if verbose:
-            print("data frames concated successfully")
-
+        return pd.concat(inputs)
         
-    def fit(self, verbose:int=0):
-        if not hasattr(self, 'sensors_dfs'):
-            raise RuntimeError("Before fitting data, first load it!")
-        for sensor in self.sensors_dfs:
-            df = self.sensors_dfs[sensor]
-            X = df[[col for col in df.columns if col != 'label']]
-            y = df['label']
-            trainer = Trainer(self.models_params, X, y, split=False)
-            trainer.fit(verbose=max(verbose-1, 0))
-            self._best_estimator[sensor] = trainer._best_estimator
-            self.X_train[sensor] = trainer.X_train
-            self.y_train[sensor] = trainer.y_train
-            if verbose: print(f'Sensor {sensor} done!')
-            del trainer
-        if verbose:print()
-
         
-    def predict(self, X_test:dict[str, list], verbose:int=0):
-        preds = {}
-        final_pred = np.zeros((len(list(X_test.values())[0]),))
-        for sensor in self._best_estimator:
-            df = X_test[sensor]
-            X = df[[col for col in df.columns if col != 'label']]
-            y = df['label']
-            pred = self._best_estimator[sensor].predict(X)
-            preds[sensor] = pred
+    def fit(self, df, verbose:int=0):
+        X = df[[col for col in df.columns if col != 'label']]
+        y = df['label']
+        trainer = Trainer(self.models_params, X, y, split=False)
+        trainer.fit(verbose=max(verbose-1, 0))
+        self._best_estimator = trainer._best_estimator
+        self.X_train = trainer.X_train
+        self.y_train = trainer.y_train
         
-        for i in range(len(preds)):
-            counts = {1: 0, 2: 0, 3: 0, 4: 0}
-            for sensor in self._best_estimator:
-                counts[preds[sensor][i]] += 1
-            popular = max(counts.items(), key=lambda x: x[1])[1] # the most vote number
-            items = [x[0] for x in counts.items() if x[1] == popular] # those with the most vote
-            final_pred[i] = random.sample(items, 1)[0] # choose one of them randomly
-        self.prediction = final_pred
-        self.y_test = y
-        return final_pred
+    def predict(self, X_test:pd.DataFrame, verbose:int=0):
+        pred = self._best_estimator.predict(X_test)
+        print(f"predicting df of size {X_test.shape}, made {pred.shape}")
+        self.prediction = pred
+        return pred
     
-    def report(self, X_test:dict[str, list]=None, verbose:int=0):
-        if not hasattr(self, 'prediction'):
-            assert X_test != None
-            self.predict(X_test=X_test, verbose=verbose)
-        report:dict = classification_report(y_true=self.y_test, y_pred=self.prediction, output_dict=True)
+    def report(self, df_test:pd.DataFrame, verbose:int=0):
+        X_test = df_test[[col for col in df_test.columns if col != 'label']]
+        y_test = df_test['label']
+        self.predict(X_test=X_test, verbose=verbose)
+        report = classification_report(y_true=y_test, y_pred=self.prediction, output_dict=True)
         return report
 
 
