ea48b8b0619f0e0c6e29c7cd85d6f2121458c2f8
alia-asl <aliabdollahiasl@gmail.com>
2024-06-17 16:39:13 +0330

    to quarter seconds

--- clean_pkl.py
+++ clean_pkl.py
@@ -12,20 +12,19 @@ def clean_pkl():
             data = pickle.load(file, encoding='bytes')
         labels:np.ndarray = data[b'label']
         signals = data[b'signal']
-        labels_sec = labels.reshape((-1, 700))
+        labels_sec = labels.reshape((-1, 700 // 4))
         tmp_data = {device: 
                     {sensor.decode(): 
-                        signals[device.encode()][sensor].reshape((-1, 3 if sensor == b'ACC' else 1, freqs[device][sensor.decode()]))
+                        signals[device.encode()][sensor].reshape((-1, 3 if sensor == b'ACC' else 1, freqs[device][sensor.decode()] // 4))
                         for sensor in signals[device.encode()]} 
                     for device in ['wrist', 'chest']}
         goods = (labels_sec.min(axis=1) > 0) & (labels_sec.max(axis=1) < 5)
         labels_sec_good = labels_sec[goods]
         end_data:dict[str, torch.Tensor] = {f"{device}_{sensor}": torch.tensor(tmp_data[device][sensor][goods]) for device in tmp_data for sensor in tmp_data[device]}
-            
         end_data['label'] = torch.tensor(labels_sec_good)
         print(end=f'\rWriting S{id}')
         with open(f"WESAD/S{id}/S{id}_n0.pkl", 'wb') as file:
             pickle.dump(end_data, file)
-        print(f'\rS{id} Finished ({labels.shape[0] / 700} -> {len(labels_sec_good)} seconds)')
+        print(f'\rS{id} Finished ({labels.shape} -> {end_data["label"].shape} seconds)')
     
 clean_pkl()
